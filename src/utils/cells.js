cebf172353b11e6e1146743a0f42077a4392ce12
Corentin BALLOT <corentin.ballot@gmail.com>
2021-05-17 23:44:13 +0200

    add regions getter

--- src/utils/cells.js
+++ src/utils/cells.js
@@ -32,6 +32,33 @@ export const isCellSurrounded = (id) => {
     return isTopOccupied && isRightOccupied && isBotOccupied && isLeftOccupied;
 }
 
+export const getRegions = (cells, cellsPerLine) => {
+    return cells.filter(cell => cell.type != CELL_TYPES.EMPTY.id).map(cell => {
+        let cs = [cell.id];
+        if(cell.id >= cellsPerLine && cells[cell.id - cellsPerLine].type == cell.type) cs.push(cell.id - cellsPerLine);
+        if(cell.id < cells.length - cellsPerLine && cells[cell.id + cellsPerLine].type == cell.type) cs.push(cell.id + cellsPerLine);
+        if(cell.id%cellsPerLine != 0 && cells[cell.id - 1].type == cell.type) cs.push(cell.id - 1);
+        if(cell.id%cellsPerLine != cellsPerLine - 1 && cells[cell.id + 1].type == cell.type) cs.push(cell.id + 1);
+
+        return {
+            type: cell.type,
+            cells: cs
+        }
+    }).reduce(regionsReducer);
+}
+
+const regionsReducer = (regions, region) => {
+    if(!Array.isArray(regions)) regions = [regions];
+    let found = false;
+    regions.forEach(r => {
+        if(r.type == region.type && r.cells.some(_r => region.cells.includes(_r))) {
+            r.cells = r.cells.concat(region.cells).filter((c, index, a) => a.indexOf(c) == index);
+            found = true;
+        }
+    });
+    return found ? regions : [...regions, region];
+}
+
 export const initBoardA = () => {
     let array = initBoard(121);
 
