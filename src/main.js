569bf80b66a2d892abb5cd20640be92dc4b84cb9
Corentin BALLOT <corentin.ballot@gmail.com>
2021-05-19 18:07:51 +0200

    remove unused props

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'Corentin',
-		title: 'World'
-	}
+	target: document.body
 });
 
 export default app;
